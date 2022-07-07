c8fc270e50e358072ccf8beb0af22953d80b1f67
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-07 16:57:47 +0900

    spring_07_01_0707

---
+++ src/main/java/com/hoon/test/MainClass.java
@@ -0,0 +1,25 @@
+package com.hoon.test;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//AbstractApplicationContext ctx = new GenericXmlApplicationContext();
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		
+		ctx.load("classpath:applicationCTx.xml");
+		
+		ctx.refresh();
+		
+		Student student = ctx.getBean("student", Student.class);
+		System.out.println(student.getName());
+		
+		ctx.close();
+		
+	}
+
+}
